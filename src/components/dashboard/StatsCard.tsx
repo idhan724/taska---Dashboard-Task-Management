cb54703824dbf6b68c5736bdbad8d46feb042ebf
Idhan Khalas Saputra <iidhankhalassaputra@gmail.com>
2026-08-09 00:30:28 +0700

    refactor: remove redundant isLoading early return in StatsCards

--- src/components/dashboard/StatsCard.tsx
+++ src/components/dashboard/StatsCard.tsx
@@ -28,16 +28,6 @@ const cards = [
 ];
 
 export function StatsCards({ stats, isLoading }: StatsCardsProps) {
-  if (isLoading) {
-    return (
-      <div className="grid grid-cols-2 lg:grid-cols-3 gap-4">
-        {[...Array(3)].map((_, i) => (
-          <Skeleton key={i} className="h-28" />
-        ))}
-      </div>
-    );
-  }
-
   return (
     <div className="grid grid-cols-2 lg:grid-cols-3 gap-4">
       {isLoading
